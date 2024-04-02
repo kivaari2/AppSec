94bbae28f7d8cfc1bc528889d463d8431839f74c
kivaari2 <165826503+kivaari2@users.noreply.github.com>
2024-04-02 20:19:11 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+import subprocess
+
+app = Flask(name)
+
+@app.route("/dns")
+def dns_lookup():
+    hostname = request.values.get('hostname')
+    cmd = 'nslookup ' + hostname
+    output = subprocess.check_output(cmd, shell=True, text=True)  # Уязвимость
+return output
+
+if name == "main":
+    app.run(debug=True)
